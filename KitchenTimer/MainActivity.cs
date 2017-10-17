ae060ac828fc1e9da3dc05214697b45514a6a3b5
Shintaro Hirasawa <nobizo@gmail.com>
2017-10-18 08:06:58 +0900

    タイマーイベントなど諸々実装

--- KitchenTimer/MainActivity.cs
+++ KitchenTimer/MainActivity.cs
@@ -1,19 +1,23 @@
-﻿using Android.App;
+﻿using System.Threading;
+using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace KitchenTimer
 {
     [Activity(Label = "KitchenTimer", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        private Timer _timer;
+        private DateTime _lastTime;
+        private enum UnitKindEnum { Minute, Second }
         private int _remainingMilliSec = 0; // 残り時間(ミリ秒)
+        private bool _isCountDown = false;  // タイマー作動中か
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
             var btn10min = FindViewById<Button>(Resource.Id.Add10MinButton);
@@ -21,40 +25,81 @@ namespace KitchenTimer
             var btn10sec = FindViewById<Button>(Resource.Id.Add10SecButton);
             var btn1sec = FindViewById<Button>(Resource.Id.Add1SecButton);
             var btnclear = FindViewById<Button>(Resource.Id.ClearButton);
+            var btnstart = FindViewById<Button>(Resource.Id.StartButton);
 
-            btn10min.Click += Btn10min_Click;
-            btn1min.Click += Btn1min_Click;
-            btn10sec.Click += Btn10sec_Click;
-            btn1sec.Click += Btn1sec_Click;
+            // 残り時間追加処理を登録
+            btn10min.Click += (sender, e) =>
+            {
+                AddRemainingTime(UnitKindEnum.Minute, 10);
+                ShowRemainingTime();
+            };
+            btn1min.Click += (sender, e) =>
+            {
+                AddRemainingTime(UnitKindEnum.Minute, 1);
+                ShowRemainingTime();
+            };
+            btn10sec.Click += (sender, e) =>
+            {
+                AddRemainingTime(UnitKindEnum.Second, 10);
+                ShowRemainingTime();
+            };
+            btn1sec.Click += (sender, e) =>
+            {
+                AddRemainingTime(UnitKindEnum.Second, 1);
+                ShowRemainingTime();
+            };
+            // 残り時間クリア処理
             btnclear.Click += (sender, e) =>
             {
                 this._remainingMilliSec = 0;
                 ShowRemainingTime();
             };
+
+            btnstart.Click += (sender, e) =>
+            {
+                this._isCountDown = !this._isCountDown;
+                this._lastTime = DateTime.Now;
+                if (this._isCountDown) { btnstart.Text = "ストップ"; }
+                else { btnstart.Text = "スタート"; }
+            };
+
+            this._lastTime = DateTime.Now;
+            this._timer = new Timer(Timer_OnTick, null, 0, 100);
         }
 
-        private void Btn1sec_Click(object sender, System.EventArgs e)
+        private void Timer_OnTick(object state)
         {
-            this._remainingMilliSec += 1000;
-            ShowRemainingTime();
+            if (!this._isCountDown) { return; }
+
+            RunOnUiThread(() =>
+            {
+                var span = DateTime.Now - this._lastTime;
+                this._remainingMilliSec -= (int)span.TotalMilliseconds;
+                if(this._remainingMilliSec <= 0)
+                {
+                    this._isCountDown = false;
+                    this._remainingMilliSec = 0;
+                    FindViewById<Button>(Resource.Id.StartButton).Text = "スタート";
+                    // アラームを鳴らす代わりにメッセージ
+                    var alert = new AlertDialog.Builder(this);
+                    alert.SetTitle("End");
+                    alert.SetMessage("終わったよ");
+                    alert.Show();
+                }
+                this._lastTime = DateTime.Now;
+                ShowRemainingTime();
+            });
         }
 
-        private void Btn10sec_Click(object sender, System.EventArgs e)
+        private void AddRemainingTime(UnitKindEnum unit, int value)
         {
-            this._remainingMilliSec += 10 * 1000;
-            ShowRemainingTime();
-        }
-
-        private void Btn1min_Click(object sender, System.EventArgs e)
-        {
-            this._remainingMilliSec += 60 * 1000;
-            ShowRemainingTime();
-        }
-
-        private void Btn10min_Click(object sender, System.EventArgs e)
-        {
-            this._remainingMilliSec += 10 * 60 * 1000;
-            ShowRemainingTime();
+            switch (unit)
+            {
+                case UnitKindEnum.Minute: { value *= 60; break; }
+                case UnitKindEnum.Second: { break; }
+            }
+            value = value * 1000;
+            this._remainingMilliSec += value;
         }
 
         private void ShowRemainingTime()
